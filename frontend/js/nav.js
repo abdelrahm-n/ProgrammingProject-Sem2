ccd19ef1a0879369e82f97d549bfc4509a99357a
abdelrahm-n <abdelrahm-n@users.noreply.github.com>
2026-06-08 15:30:00 +0200

    chore: debug-log uit navigatiescript verwijderen

--- frontend/js/nav.js
+++ frontend/js/nav.js
@@ -1,7 +1,5 @@
 // nav.js - navigatielogica voor alle pagina's
 
-console.log('nav.js geladen')
-
 // stuur door naar index als er geen rol is
 if (!localStorage.getItem('rol')) {
   window.location.href = 'index.html'
